db3a7822e379aeadbf8433be3fab760db1db53df
mataeo-eh <mataeo.eh@gmail.com>
2026-01-16 07:14:31 -0600

    Improved robustness and error handling of reading environment variables

--- bot_core.py
+++ bot_core.py
@@ -9,11 +9,18 @@ from dotenv import load_dotenv
 load_dotenv()
 try:
     COC_API_key = os.getenv("COC_API_KEY")
+    if COC_API_key is None:
+        raise ValueError("COC_API_KEY environment variable not set")
     Discord_Bot_API_Key = os.getenv("DISCORD_BOT_API_KEY")
+    if Discord_Bot_API_Key is None:
+        raise ValueError("DISCORD_BOT_API_KEY environment variable not set")
     Discord_bot_test_guild_ID = os.getenv("DISCORD_BOT_TEST_GUILD_ID")
+    if Discord_bot_test_guild_ID is None:
+        raise ValueError("DISCORD_BOT_TEST_GUILD_ID environment variable not set")
 except Exception as e:
     print(f"Error loading environment variables: {e}")
     raise
+
 
 
 from COC_API import CoCAPI
